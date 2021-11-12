eb6c02c23e22efc46a73be45bee5313f45b4550c
Vashisht Marhwal <22131948+vashishtmarhwal@users.noreply.github.com>
2021-11-11 22:03:49 -0500

    added model.pt

--- mainGPU.py
+++ mainGPU.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+from torchvision.utils import make_grid
 import cv2
 import seaborn as sn
 import pandas as pd
@@ -17,7 +18,68 @@ import torch.nn as nn
 import torch.nn.functional as functional
 import torch.optim as optim
 import warnings
+import torch.nn.functional as F
 warnings.filterwarnings('ignore')
+
+class ImageClassificationBase(nn.Module):
+    
+    def training_step(self, batch):
+        images, labels = batch 
+        out = self(images)                  # Generate predictions
+        loss = F.cross_entropy(out, labels) # Calculate loss
+        return loss
+    
+    def validation_step(self, batch):
+        images, labels = batch 
+        out = self(images)                    # Generate predictions
+        loss = F.cross_entropy(out, labels)   # Calculate loss
+        acc = accuracy(out, labels)           # Calculate accuracy
+        return {'val_loss': loss.detach(), 'val_acc': acc}
+        
+    def validation_epoch_end(self, outputs):
+        batch_losses = [x['val_loss'] for x in outputs]
+        epoch_loss = torch.stack(batch_losses).mean()   # Combine losses
+        batch_accs = [x['val_acc'] for x in outputs]
+        epoch_acc = torch.stack(batch_accs).mean()      # Combine accuracies
+        return {'val_loss': epoch_loss.item(), 'val_acc': epoch_acc.item()}
+    
+    def epoch_end(self, epoch, result):
+        print("Epoch [{}], train_loss: {:.4f}, val_loss: {:.4f}, val_acc: {:.4f}".format(
+            epoch, result['train_loss'], result['val_loss'], result['val_acc']))
+
+class NaturalSceneClassification(ImageClassificationBase):
+    def __init__(self):
+        super().__init__()
+        self.network = nn.Sequential(
+            
+            nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.LeakyReLU(inplace=True),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.LeakyReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.LeakyReLU(inplace=True),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.LeakyReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(inplace=True),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+    
+    def forward(self, xb):
+        return self.network(xb)
+
 
 
 data_dir = './FinalDataset'
@@ -26,16 +88,16 @@ def load_split_train_test(datadir, valid_size = .2):
     train_transforms = transforms.Compose([#transforms.RandomRotation(30),  # data augmentations are great
                                        #transforms.RandomResizedCrop(224),  # but not in this case of map tiles
                                        #transforms.RandomHorizontalFlip(),
-                                       transforms.Resize(224),
+                                       transforms.Resize(128),
                                        transforms.ToTensor(),
-                                       transforms.Normalize([0.485, 0.456, 0.406], # PyTorch recommends these but in this
-                                                            [0.229, 0.224, 0.225]) # case I didn't get good results
+                                       #transforms.Normalize([0.485, 0.456, 0.406], # PyTorch recommends these but in this
+                                       #                     [0.229, 0.224, 0.225]) # case I didn't get good results
                                        ])
 
-    test_transforms = transforms.Compose([transforms.Resize(224),
+    test_transforms = transforms.Compose([transforms.Resize(128),
                                       transforms.ToTensor(),
-                                      transforms.Normalize([0.485, 0.456, 0.406],
-                                                           [0.229, 0.224, 0.225])
+                                      #transforms.Normalize([0.485, 0.456, 0.406],
+                                      #                     [0.229, 0.224, 0.225])
                                       ])
 
     train_data = ImageFolder(datadir, transform=train_transforms)
@@ -57,12 +119,64 @@ trainloader, testloader = load_split_train_test(data_dir, .2)
 print(trainloader.dataset.classes)
 
 def display_img(img,label):
-    print(f"Label : {dataset.classes[label]}")
-    plt.imshow(image.numpy().transpose((1, 2, 0)))
+    print(f"Label : {trainloader.dataset.classes[label]}")
+    plt.imshow(img.permute(1,2,0))
+    plt.show()
 
 #display the first image in the dataset
-dataiter = iter(trainloader)
-images, labels = dataiter.next()
-display_img(images[0], labels[0])
+display_img(*trainloader.dataset[0])
+
+def show_batch(dl):
+    """Plot images grid of single batch"""
+    for images, labels in dl:
+        fig,ax = plt.subplots(figsize = (16,12))
+        ax.set_xticks([])
+        ax.set_yticks([])
+        ax.imshow(make_grid(images,nrow=16).permute(1,2,0))
+        plt.show()
+        break
+        
+show_batch(trainloader)
+
+def accuracy(outputs, labels):
+    _, preds = torch.max(outputs, dim=1)
+    return torch.tensor(torch.sum(preds == labels).item() / len(preds))
+
+  
+@torch.no_grad()
+def evaluate(model, val_loader):
+    model.eval()
+    outputs = [model.validation_step(batch) for batch in val_loader]
+    return model.validation_epoch_end(outputs)
+
+  
+def fit(epochs, lr, model, train_loader, val_loader, opt_func = torch.optim.SGD):
+    
+    history = []
+    optimizer = opt_func(model.parameters(),lr)
+    for epoch in range(epochs):
+        
+        model.train()
+        train_losses = []
+        for batch in train_loader:
+            loss = model.training_step(batch)
+            train_losses.append(loss)
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+            
+        result = evaluate(model, val_loader)
+        result['train_loss'] = torch.stack(train_losses).mean().item()
+        model.epoch_end(epoch, result)
+        history.append(result)
+    
+    return history
+
+    
+num_epochs = 30
+opt_func = torch.optim.Adam
+lr = 0.001#fitting the model on training data and record the result after each epoch
+model = NaturalSceneClassification()
+history = fit(num_epochs, lr, model, trainloader, testloader, opt_func)
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
