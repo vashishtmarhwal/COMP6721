87a547c78cfdbef4a2e00509e078fd352b0246f9
Vashisht Marhwal <22131948+vashishtmarhwal@users.noreply.github.com>
2021-12-05 18:03:15 -0500

    bash

---
+++ MaskFaceDetection.sh
@@ -0,0 +1,45 @@
+#!/encs/bin/tcsh
+
+#####################################################################################################################
+## Gurobi brings its own version of Python but that one does not contain any 3rd-party Python packages except Gurobi. 
+## In order to use Gurobi together with popular Python packages like multiprocessing and others, we need to create a
+## virtual Python environment, in which we can install gurobipy and other pakages.
+##
+## You can create the new virtual Python environment inside your home directory before scheduling your job,
+## or create it inside $TMPDIR on fly as a part of your job
+#####################################################################################################################
+
+#$ -N MY_JOB
+#$ -cwd
+#$ -m bea
+#$ -pe smp 8
+#$ -l h_vmem=150G
+
+##PUT YOUR MODULE LOADS HERE
+module load gurobi/9.0.2/default
+module load python/3.7.7/default
+
+## Create a virtual Python environment (env) in $TMPDIR
+python3.7 -m venv $TMPDIR/env
+## Activate the new environment
+source $TMPDIR/env/bin/activate.csh
+## Install gurobipy module
+cd $GUROBI_HOME
+python3.7 setup.py build --build-base /tmp/${USER} install
+
+## return to workDir
+cd $SGE_O_WORKDIR
+
+## Now, instead of using 'gurobi.sh MY_PYTHON_SCRIPT.py', you can use
+pip install numpy
+pip install matplotlib
+pip install --upgrade pip setuptools wheel
+pip install opencv-python
+pip install seaborn
+pip install torch
+pip install torchvision
+pip install sklearn
+python main.py
+## inside MY_PYTHON_SCRIPT.py, you can use
+## from gurobipy import *
+## import multiprocessing as mp
